aa87243d3e134fb67261b130cf7f7e36ec1b8cfe
volterr <volterr@gmail.com>
2014-06-21 21:26:31 +0400

    cached solve implementation ready for testing

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions are the realization of cached solve() function.
+## It allows to cache inverse matrix, saving computation time if the results
+## will be requested second time
 
-## Write a short comment describing this function
+## makeCacheMatrix creates implementation of matrix 
+## with ability to cache solve() result
+##
+## s - cached solve result
+## x - matrix data
+##
+## returns the list of 4 functions:
+##  *set - allows to set new matrix data themselves
+##  *get - returns matrix data
+##  *getsolve - returns cached inverse matrix if any
+##  *setsolve - allows to set inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
+    
+    list(set = set, get = get, 
+         setsolve = setsolve, getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function is the implementation of solve function
+## with ability to cache results in the matrix created with makeCacheMatrix function
+##
+## if input variable x is created with makeCacheMatrix then solve result is cached
+## if input variable x is matrix then cacheSolve just returns the solve result
+##
+## returns inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    if( class(x) == "list" ){   # x was probably created with makeCacheMatrix        
+        # check for cached results
+        s <- x$getsolve()
+        if(!is.null(s)) {
+            message("getting cached data")
+            return(s)
+        }
+        
+        # apply solve function and cache results
+        data <- x$get()
+        s <- solve(data, ...)     
+        x$setsolve(s) 
+        
+    } else if( class(x) == "matrix" ){  # x is a matrix      
+        s <- solve(x, ...)  
+        
+    } else {
+        message("input should be either list created with makeCacheMatrix, either matrix itself")
+        s <- NULL
+    }
+    
+    s
 }
